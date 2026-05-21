b458c6ae446d5e786e5b9a84ee46d204d230104b
Anderson Brendon <andersonbrendonn@gmail.com>
2026-05-21 15:40:24 -0300

    refactor(UserAnswerDTO): rename choosedOption to selectedOption

--- src/main/java/com/gamifiedlibrary/api/infrastructure/dto/quiz/UserAnswerDTO.java
+++ src/main/java/com/gamifiedlibrary/api/infrastructure/dto/quiz/UserAnswerDTO.java
@@ -1,5 +1,5 @@
 package com.gamifiedlibrary.api.infrastructure.dto.quiz;
 
-public record UserAnswerDTO(Long questionId, boolean randomSelection, String choosedOption, String correctOption) {
+public record UserAnswerDTO(Long questionId, boolean randomSelection, String selectedOption, String correctOption) {
 
 }
